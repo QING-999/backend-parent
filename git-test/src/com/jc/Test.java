ab72728b113c08d1e04e7021d6ff5cfda0ca165c
admin <1578340873@qq.com>
2020-08-03 01:33:00 +0800

    添加test文件

---
+++ git-test/src/com/jc/Test.java
@@ -0,0 +1,12 @@
+package com.jc;
+
+/**
+ * @Author CJ
+ * @Date 2020/8/3 1:21
+ * Description
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
